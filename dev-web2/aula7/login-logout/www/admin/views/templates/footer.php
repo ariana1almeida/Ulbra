fc18c8abbc490466952f5f51ded71b505b441904
ariana1almeida <arianaleu@hotmail.com>
2022-09-14 14:48:48 -0300

    enviando a tde da aula 7

---
+++ dev-web2/aula7/login-logout/www/admin/views/templates/footer.php
@@ -0,0 +1,10 @@
+        </section>
+        </div>
+        </div>
+
+        <footer class="text-center p-3">
+            <p>All rights reserved to ULBRA</p>
+            <a href="../index.php">Site</a>
+        </footer>
+    </body>
+    </html>
---
+++ dev-web2/aula7/login-logout/www/admin/views/templates/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalho PW</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/script.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="p-3 text-center">
+        <h1>Área Administrativa</h1>
+        <a href="?controller=main&action=logout">Sair</a>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="p-3 col-md-3">
+                <h2>Menu</h2>
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a href="?controller=main&action=index" class="nav-link">Home</a>
+                    </li>
+                </ul>
+            </nav>
+            <section class="p-3 col-md-9">
